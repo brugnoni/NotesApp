3f580a192107be932be25abb3c5966da9bf1b3a3
Nahuel Brugnoni <nahubrugnoni@gmail.com>
2023-07-27 20:18:55 -0300

    NotesApp

---
+++ src/Pages/NotesEditionPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotesEditionPage = () => {
+  return <div>Esta es la página de edición</div>;
+};
+
+export default NotesEditionPage;
---
+++ src/Pages/NotesListPage.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+
+const NotesListPage = () => {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    getNotes();
+  }, []);
+
+  const getNotes = async () => {
+    let res = await fetch("http://127.0.0.1:8000/notes");
+    let data = await res.json();
+    console.log("DATA: ", data);
+    setNotes(data);
+  };
+
+  return <div>{notes}</div>;
+};
+
+export default NotesListPage;
