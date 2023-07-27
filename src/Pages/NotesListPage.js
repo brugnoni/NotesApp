bbd366eb37034b08045ab7375e455b18fa6cdc2d
Nahuel Brugnoni <nahubrugnoni@gmail.com>
2023-07-27 20:21:44 -0300

    NotesApp

--- src/Pages/NotesListPage.js
+++ src/Pages/NotesListPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, setNotes } from "react";
 
 const NotesListPage = () => {
   const [notes, setNotes] = useState([]);
