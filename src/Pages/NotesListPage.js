1243362206591f308f379f86707b2a4065b5bffd
Nahuel Brugnoni <nahubrugnoni@gmail.com>
2023-07-27 23:27:20 -0300

    Checkpoint 0% + Babel Cfg

--- src/Pages/NotesListPage.js
+++ src/Pages/NotesListPage.js
@@ -1,20 +1,28 @@
 import React, { useEffect, useState } from "react";
+import ListItem from "../Components/ListItem";
 
 const NotesListPage = () => {
   const [notes, setNotes] = useState([]);
 
   useEffect(() => {
     getNotes();
-  }, [notes]);
+  }, []);
 
   const getNotes = async () => {
-    let res = await fetch("http://127.0.0.1:8000/notes");
+    let res = await fetch("http://127.0.0.1:8000/notes/");
     let data = await res.json();
-    console.log("DATA: ", data);
     setNotes(data);
   };
 
-  return <div>notes</div>;
+  return (
+    <div>
+      <div className="notes-list">
+        {notes.map((note, index) => (
+          <ListItem key={index} note={note} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default NotesListPage;
